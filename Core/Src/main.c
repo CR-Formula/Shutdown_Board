6dbbf0d8fc7a95e755890bec12f932dff9dbe8fb
APBashara <alexbashara2002@gmail.com>
2025-03-07 23:13:52 -0800

    Inital Commit

---
+++ Core/Src/main.c
@@ -0,0 +1,13 @@
+/************************************************
+* @file    main.c 
+* @author  APBashara
+* @date    3/2025
+* 
+* @brief   Main Code to run Shutdown Board
+***********************************************/
+
+#include "main.h"
+
+void main() {
+
+}
---
+++ Core/Src/uart.c
@@ -0,0 +1,7 @@
+/************************************************
+* @file    uart.c 
+* @author  APBashara
+* @date    9/2024
+* 
+* @brief   Driver implementation for UART
+***********************************************/
